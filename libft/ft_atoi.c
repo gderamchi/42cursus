8052ad0f70920bb0d533b9ac05545a23262ac99f
Scamir <gderamchi@gmail.com>
2025-10-04 20:41:47 +0200

    added atoi strcmp and strlen

---
+++ libft/ft_atoi.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: guillaume_deramchi <guillaume_deramchi@    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/04 05:23:21 by guillaume_d       #+#    #+#             */
+/*   Updated: 2025/10/04 20:21:21 by guillaume_d      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	res;
+	int	sign;
+
+	i = 0;
+	res = 0;
+	sign = 0;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign++;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res = res * 10 + str[i] - '0';
+		i++;
+	}
+	if (sign % 2)
+		res *= -1;
+	return (res);
+}
+
+int	main(int ac, char **av)
+{
+	printf("%d\n", ft_atoi(av[1]));
+	return (0);
+}
